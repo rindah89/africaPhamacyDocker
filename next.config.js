7050db769a6ab8a8b31bf685047142810c53d7b0
Rindah Ngwanyam <ngwanyam@gmail.com>
2025-02-01 10:28:00 +0100

    manifest

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**',
       },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
     ],
   },
   typescript: {
